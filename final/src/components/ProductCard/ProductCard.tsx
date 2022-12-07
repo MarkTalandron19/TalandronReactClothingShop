ad199225794b9aa3ce746f9d6f4feed74cd5baf4
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2022-12-07 15:29:47 +0800

    implemented still buggy cart

--- final/src/components/ProductCard/ProductCard.tsx
+++ final/src/components/ProductCard/ProductCard.tsx
@@ -6,13 +6,28 @@ import {
   Wrapper,
 } from './ProductCard.styled';
 
+import { useState, useEffect, useContext } from 'react';
 import { Product } from '../../models';
+import { ShopContext } from '../../context';
 
 export const ProductCard = ({ name, imageUrl, price }: Product) => {
+  const {addToCart, removeFromCart} = useContext(ShopContext);
+  const [isInCart, setIsInCart] = useState(false);
+  
+  const handleClick = () => {
+    const product = {name, imageUrl, price};
+    if(isInCart){
+      removeFromCart(product);
+      setIsInCart(false);
+    } else{
+      addToCart(product);
+      setIsInCart(true);
+    }
+  }
   return (
     <Wrapper background={imageUrl}>
-      <AddButton isInCart={false} onClick={() => console.log('Implement Me')}>
-        <p>+</p>
+      <AddButton isInCart={isInCart} onClick={handleClick}>
+        <p>{isInCart? "-" : "+"}</p>
       </AddButton>
       <TextContainer>
         <Title>{name}</Title>
